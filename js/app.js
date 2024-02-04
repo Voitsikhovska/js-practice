1f457a6ea681be59a587faff122f4692d63fa109
Voitsikhovska <daravojka505@gmail.com>
2024-02-04 21:16:48 +0200

    creating a pyramid

--- js/app.js
+++ js/app.js
@@ -35,3 +35,21 @@ const lastFilm = prompt('One of the last films you watched?','');
 const ratingFilm = prompt('How much would you rate it?','');
 personalMovieDB.movies[lastFilm]=ratingFilm;
 console.log(personalMovieDB);
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i < lines; i++) {
+  for (let j = 0; j < lines - i +1; j++) {
+    result += " ";
+
+  }
+  for (let j = 0; j < 2 * i + 1; j++) {
+    result += "*";
+
+  }
+  result += "\n";
+
+}
+
+console.log(result);
