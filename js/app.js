4194aaff0af2d2eec5246d337af85ba93c37aed4
Voitsikhovska <daravojka505@gmail.com>
2024-02-05 17:05:51 +0200

    js task 3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 
 'use strict';
-const numberOfFilms = +prompt('How many movies have you watched?');
+let numberOfFilms;
+function start(){
+ numberOfFilms = +prompt('How many movies have you watched?');
+ while(numberOfFilms == ''|| numberOfFilms==null||isNaN(numberOfFilms)){
+   numberOfFilms = +prompt('How many movies have you watched?');
+ }
+}
+start();
 const personalMovieDB = {
   count:numberOfFilms,
   movies:{},
@@ -8,30 +15,46 @@ const personalMovieDB = {
   genres:[],
   privat:false
 };
-
-for (let i=0; i<2;i++){
-  const lastFilm = prompt('One of the last films you watched?','');
-  const ratingFilm = prompt('How much would you rate it?','');
-  if (lastFilm !=null && ratingFilm!= null && lastFilm !='' && ratingFilm !='' && lastFilm.length <50 && ratingFilm.length<50){
-    personalMovieDB.movies[lastFilm]=ratingFilm;
+function rememberMyFilms(){
+  for (let i=0; i<2;i++){
+    const lastFilm = prompt('One of the last films you watched?','');
+    const ratingFilm = prompt('How much would you rate it?','');
+    if (lastFilm !=null && ratingFilm!= null && lastFilm !='' && ratingFilm !='' && lastFilm.length <50 && ratingFilm.length<50){
+      personalMovieDB.movies[lastFilm]=ratingFilm;
+    }
+    else {
+      i--;
+    }
   }
-  else {
-    i--;
+}
+rememberMyFilms();
+
+function detectPersonalLevel(){
+  if(personalMovieDB.count<10){
+    alert("too malo films");
+  } else if(personalMovieDB.count>=10&&personalMovieDB.count <30){
+    alert('you are classic');
+  }
+  else if(personalMovieDB.count>30){
+    alert('ypu are kinoman');
+  }
+  else {alert('error')}
+  console.log(personalMovieDB);
+}
+detectPersonalLevel();
+function showMyDB(hidden){
+  if(!hidden){
+    console.log(personalMovieDB) ;
   }
 }
+showMyDB(personalMovieDB.privat);
+function writeYourGenres(variable){
+ for (let i=1;i<=3;i++) {
+   personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
+ }
 
-console.log(personalMovieDB);
-
-if(personalMovieDB.count<10){
-  alert("too malo films");
-} else if(personalMovieDB.count>=10&&personalMovieDB.count <30){
-  alert('you are classic');
 }
-else if(personalMovieDB.count>30){
-  alert('ypu are kinoman');
-}
-else {alert('error')}
-
+writeYourGenres()
 /*const lines = 5;
 let result = '';
 
