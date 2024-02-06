16bb1d6b48696603419032088ea5995fb77810a7
Voitsikhovska <daravojka505@gmail.com>
2024-02-06 10:30:55 +0200

    practice with array (1)

--- js/practiceObject.js
+++ js/practiceObject.js
@@ -2,13 +2,14 @@ const personalPlanPeter = {
   name: "Peter",
   age: "29",
   skills: {
-    languages: ['ru', 'eng'],
+    languages: ['uk', 'eng'],
     programmingLangs: {
       js: '20%',
       php: '10%'
     },
     exp: '1 month'
   },
+  //task3
   showAgeAndLang:function (plan){
 const{age}=plan;
 const {languages}=plan.skills
@@ -20,12 +21,13 @@ return console.log(str)
   }
 };
 personalPlanPeter.showAgeAndLang(personalPlanPeter);
+//task1
 function showExperience(plan) {
   const {exp} = plan.skills;
   return console.log(exp)
 }
 showExperience(personalPlanPeter);
-
+//task2
 function showProgrammingLangs(plan) {
   let str ='';
   const {programmingLangs}= plan.skills;
@@ -35,3 +37,29 @@ function showProgrammingLangs(plan) {
   console.log(str);
 }
 showProgrammingLangs(personalPlanPeter)
+
+
+
+//ARRAY PRACTICE
+//task1
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+for (let value of arr){
+}
+return console.log(`family are : ${arr}`)
+}
+showFamily(family)
+
+//task2
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+function standartizeString(array){
+
+  /*array.forEach(city=>{
+    console.log(city.toLowerCase());
+  })*/
+  for(let i=0;i<array.length;i++){
+    console.log(array[i].toLowerCase())
+  }
+}
+standartizeString(favoriteCities)
