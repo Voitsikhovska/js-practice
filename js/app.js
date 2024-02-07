4dcc08b5bd05d59227dc5d0a5f6e6eb62212f145
Voitsikhovska <daravojka505@gmail.com>
2024-02-07 22:31:25 +0200

    js task 4

--- js/app.js
+++ js/app.js
@@ -1,60 +1,82 @@
 
 'use strict';
-let numberOfFilms;
-function start(){
- numberOfFilms = +prompt('How many movies have you watched?');
- while(numberOfFilms == ''|| numberOfFilms==null||isNaN(numberOfFilms)){
-   numberOfFilms = +prompt('How many movies have you watched?');
- }
-}
-start();
+
+
 const personalMovieDB = {
-  count:numberOfFilms,
+  count:0,
   movies:{},
   actors:{},
   genres:[],
-  privat:false
-};
-function rememberMyFilms(){
-  for (let i=0; i<2;i++){
-    const lastFilm = prompt('One of the last films you watched?','').trim();
-    const ratingFilm = prompt('How much would you rate it?','');
-    if (lastFilm !=null && ratingFilm!= null && lastFilm !='' && ratingFilm !='' && lastFilm.length <50 && ratingFilm.length<50){
-      personalMovieDB.movies[lastFilm]=ratingFilm;
+  privat:false,
+  start: function(){//method
+    personalMovieDB.count= +prompt('How many movies have you watched?');
+    while(personalMovieDB.count == ''|| personalMovieDB.count==null||isNaN(personalMovieDB.count)){
+      personalMovieDB.count= +prompt('How many movies have you watched?');
+    }},
+  rememberMyFilms: function (){ //method
+    for (let i=0; i<2;i++){
+      const lastFilm = prompt('One of the last films you watched?','').trim();
+      const ratingFilm = prompt('How much would you rate it?','');
+      if (lastFilm !=null && ratingFilm!= null && lastFilm !='' && ratingFilm !='' && lastFilm.length <50 && ratingFilm.length<50){
+        personalMovieDB.movies[lastFilm]=ratingFilm;
+      }
+      else {
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function (){ //method
+    if(personalMovieDB.count<10){
+      alert("too malo films");
+    } else if(personalMovieDB.count>=10&&personalMovieDB.count <30){
+      alert('you are classic');
+    }
+    else if(personalMovieDB.count>30){
+      alert('ypu are kinoman');
+    }
+    else {alert('error')}
+    console.log(personalMovieDB);
+  },
+  showMyDB:function (hidden){ //method
+    if(!hidden){
+      console.log(personalMovieDB) ;
+    }
+  },
+  writeYourGenres: function (variable){ //method
+   /* for (let i=1;i<=3;i++) {
+      let genre = prompt(`your favorite zhanr on number ${i}`);
+      if (genre === '' || genre==null){
+        console.log('wrong data')
+        i--;
+      }
+      else {
+        personalMovieDB.genres[i-1]=genre;
+      }
+    }*/
+    for (let i = 1; i < 2; i++) {
+      let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+      if (genre === '' || genre == null) {
+        console.log('Вы ввели некорректные данные или не ввели их вовсе');
+        i--;
+      } else {
+        personalMovieDB.genres[i - 1] = genre;
+      }
+    }
+      personalMovieDB.genres.forEach((item,i)=> {
+        console.log(`favourite zhanr ${i + 1} - it's ${item}`);
+      });
+
+  },
+  toggleVisibleMyDB: function (){ //method
+    if(personalMovieDB.privat){
+      personalMovieDB.privat = false;
     }
     else {
-      i--;
+      personalMovieDB.privat = true;
     }
   }
-}
-rememberMyFilms();
-
-function detectPersonalLevel(){
-  if(personalMovieDB.count<10){
-    alert("too malo films");
-  } else if(personalMovieDB.count>=10&&personalMovieDB.count <30){
-    alert('you are classic');
-  }
-  else if(personalMovieDB.count>30){
-    alert('ypu are kinoman');
-  }
-  else {alert('error')}
-  console.log(personalMovieDB);
-}
-detectPersonalLevel();
-function showMyDB(hidden){
-  if(!hidden){
-    console.log(personalMovieDB) ;
-  }
-}
-showMyDB(personalMovieDB.privat);
-function writeYourGenres(variable){
- for (let i=1;i<=3;i++) {
-   personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
- }
-
-}
-writeYourGenres()
+};
 
 
 
