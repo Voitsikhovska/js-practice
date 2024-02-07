acd1a59468846aaa072442aa9b62ac4fba481427
Voitsikhovska <daravojka505@gmail.com>
2024-02-07 22:59:55 +0200

    practice with object(2)

--- js/app.js
+++ js/app.js
@@ -79,6 +79,29 @@ const personalMovieDB = {
 };
 
 
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+  arr.sort();
+  let a =[],b=[],c=[],rest=[];
+  for (let i=0; i<arr.length;i++){
+    if(i<3){
+      a.push(arr[i]);
+    }
+    else if(i<6){
+      b.push(arr[i]);
+    }
+    else if(i<9){
+      c.push(arr[i]);
+    }
+    else {
+      rest.push(arr[i]);
+    }
+  }
+  return console.log([a,b,c, `another students: ${rest.length === 0 ? '-' : rest.join(', ')}`])
+}
+
+sortStudentsByGroups(students)
 
 /*const lines = 5;
 let result = '';
--- js/practiceObject.js
+++ js/practiceObject.js
@@ -100,3 +100,46 @@ arr.length === 0 ? str='No available currencies' : str='Available currencies'
   console.log(str)
 }
 availableCurr(additionalCurrencies,'UAH')
+
+
+//task5
+const shoppingMallData = {
+  shops: [
+    {
+      width: 10,
+      length: 5
+    },
+    {
+      width: 15,
+      length: 7
+    },
+    {
+      width: 20,
+      length: 5
+    },
+    {
+      width: 8,
+      length: 10
+    }
+  ],
+  height: 5,
+  moneyPer1m3: 30,
+  budget: 50000
+}
+
+function isBudgetEnough(data) {
+  let square = 0;
+  let volume = 0;
+
+  data.shops.forEach(shop => {
+    square += shop.width * shop.length;
+  });
+
+  volume = data.height * square;
+
+  if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+    return 'Бюджета достаточно';
+  } else {
+    return 'Бюджета недостаточно';
+  }
+}
